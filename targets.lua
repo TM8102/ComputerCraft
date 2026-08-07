24441b00bcba5aa41f0e1ba621083dd15208b292
TM8102 <218325423+TM8102@users.noreply.github.com>
2026-08-07 13:50:58 -0400

    Auto update 2026-08-07 13:50:58

--- targets.lua
+++ targets.lua
@@ -128,5 +128,11 @@ return {
         target = 65500,
 
         machine = nil
+    },
+    ["mincraft:dirt"] = {
+        displayName = "Test",
+        target = 500,
+
+        machine = nil
     }
 }
