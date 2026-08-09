45cc9a05c8b8a1f0318a36d49b4d2b18cc24ba12
TM8102 <218325423+TM8102@users.noreply.github.com>
2026-08-09 02:40:09 -0400

    Auto update 2026-08-09 02:40:09

--- targets.lua
+++ targets.lua
@@ -287,42 +287,15 @@ return {
     },
     ["allthemodium:allthemodium_ingot"] = {
         displayName = "AllTheModium Ingot",
-        target = 2097152000,
-
-        machine = {
-            computerID = 41,
-            machineKey = "allthemodium_ingot",
-            side = "right",
-
-            startBelow = 50,
-            stopAt = 100
-        }
+        target = 2097152000
     },
     ["allthemodium:vibranium_ingot"] = {
         displayName = "Vibranium Ingot",
-        target = 2097152000,
-
-        machine = {
-            computerID = 501,
-            machineKey = "vibranium_ingot",
-            side = "back",
-
-            startBelow = 50,
-            stopAt = 100
-        }
+        target = 2097152000
     },
     ["allthemodium:unobtainium_ingot"] = {
         displayName = "Unobtainium Ingot",
-        target = 2097152000,
-
-        machine = {
-            computerID = 501,
-            machineKey = "unobtainium_ingot",
-            side = "right",
-
-            startBelow = 50,
-            stopAt = 100
-        }
+        target = 2097152000
     },
     ["justdirethings:polymorphic_fluid_source"] = {
         displayName = "Polymorphic Fluid",
